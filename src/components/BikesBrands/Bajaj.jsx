6facc151c4d4ca4502e7bb1ae636beba2c4b1ee2
Bidhu1024 <bidhu1024av@gmail.com>
2023-02-12 13:10:32 +0530

    Logo Redifined

--- src/components/BikesBrands/Bajaj.jsx
+++ src/components/BikesBrands/Bajaj.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import BikeMap from './BikeMap';
 
 const Bajaj = () => {
+  const bajaj = [
+    {
+      id: 1,
+      name: "Pulsar 150 ",
+      image:
+        "https://cdn.bajajauto.com/-/media/assets/bajajauto/360degreeimages/bikes/pulsar/pulsar-150/single-disc/black-red/00.png",
+      desc: "bike desc",
+      price: 193454,
+    },
+    {
+      id: 2,
+      name: "Bajaj RS 200",
+      image:
+        "https://www.drivespark.com/bikes-photos/models/750x550/bajajpulsarrs200_1666956907.jpg/3/x.pagespeed.ic.LUa3d2w5Yg.jpg",
+      desc: "classic bike desc",
+      price: 213454,
+    },
+    {
+      id: 3,
+      name: "Bajaj Dominar 400",
+      image:
+        "https://bd.gaadicdn.com/processedimages/bajaj/dominar-400-ug/640X309/dominar-400-ug61767d8504389.jpg",
+      desc: "classic bike desc",
+      price: 213454,
+    },
+  ];
+
   return (
-    <div>Bajaj</div>
+   <BikeMap arr={bajaj}/>
   )
 }
 
