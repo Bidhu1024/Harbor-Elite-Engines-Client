bcc2ae80d4d3b50e46a253f97a9a64fa23b9d531
Asutosh Acharya <asutosh246@gmail.com>
2023-02-14 00:56:02 +0530

    routes added

--- src/components/Dashboard/EVBox.jsx
+++ src/components/Dashboard/EVBox.jsx
@@ -1,23 +1,21 @@
 import React from "react";
-import { Box,Stack,Typography } from "@mui/material";
-import ev from '../../Assets/ola.jpg'
+import { Box, Stack, Typography } from "@mui/material";
+import ev from "../../Assets/ola.jpg";
 const EVBox = () => {
   return (
-<Stack>
-<Box
-      style={{
-        backgroundImage: `url(${ev})`,
-        backgroundRepeat: 'no-repeat',
-        objectFit:'contain',
-         width:'100%' , 
-         height:'20rem'
-      }}
-    >
-<Typography>
-Evs
-</Typography>
-    </Box>
-</Stack>
+    <Stack>
+      <Box
+        style={{
+          backgroundImage: `url(${ev})`,
+          backgroundRepeat: "no-repeat",
+          objectFit: "contain",
+          width: "100%",
+          height: "20rem",
+        }}
+      >
+        <Typography>Evs</Typography>
+      </Box>
+    </Stack>
   );
 };
 
