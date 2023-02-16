5067ff73c0f9f5eb6a7b5ab559d273415c84b04f
Bidhu1024 <bidhu1024av@gmail.com>
2023-02-16 07:55:03 +0530

    Bikes page re designed

--- src/components/AllBikes/BikeBrands.jsx
+++ src/components/AllBikes/BikeBrands.jsx
@@ -14,66 +14,9 @@ const BikeBrands = () => {
   if (data) {
     console.log(data);
   }
-  if(error){
+  if (error) {
     console.log(error);
   }
-  // const AllBike = [
-  //   {
-  //     id: 1,
-  //     name: "Royal Enfield",
-  //     image:
-  //       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEvpE32OzpugPbkDd1QE7Je4OM0-mNMeemBY7lGto&s",
-  //     origin: "India",
-  //   },
-  //   {
-  //     id: 2,
-  //     name: "Yamaha",
-  //     image:
-  //       "https://logos-world.net/wp-content/uploads/2020/10/Yamaha-Logo.png",
-  //     origin: "Japan",
-  //   },
-  //   {
-  //     id: 3,
-  //     name: "TVS",
-  //     image: "https://m.media-amazon.com/images/I/71MyxHA8moL._SL1500_.jpg",
-  //     origin: "India",
-  //   },
-  //   {
-  //     id: 4,
-  //     name: "Suzuki",
-  //     image:
-  //       "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Suzuki_logo_2.svg/2560px-Suzuki_logo_2.svg.png",
-  //     origin: "India",
-  //   },
-  //   {
-  //     id: 5,
-  //     name: "Honda",
-  //     image:
-  //       "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Honda_Logo.svg/2552px-Honda_Logo.svg.png",
-  //     origin: "India",
-  //   },
-  //   {
-  //     id: 6,
-  //     name: "BMW",
-  //     image:
-  //       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWxRhW9cUG0nwf6iCNKhaVvBzd6G4nQaXsdnJRpsR-3w&s",
-  //     origin: "India",
-  //   },
-  //   {
-  //     id: 7,
-  //     name: "Bajaj",
-  //     image:
-  //       "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Bajaj_Auto_Ltd_logo.svg/1200px-Bajaj_Auto_Ltd_logo.svg.png",
-  //     origin: "India",
-  //   },
-  //   {
-  //     id: 8,
-  //     name: "KTM",
-  //     image:
-  //       "https://thumbs.dreamstime.com/b/logo-ktm-logo-ktm-bike-color-vector-format-aviable-ai-132135826.jpg",
-  //     origin: "India",
-  //   },
-  // ];
 
   if (isLoading) {
     return (
@@ -108,19 +51,24 @@ const BikeBrands = () => {
       {data.map((bike) => {
         return (
           <Box>
-            <img src={bike.image} alt="Avatar" style={{height:'17rem', width:'17rem'}}/>
-            <Box sx={{display:'flex', alignItems:'center', flexDirection:'column'}}>
-            <Box>
-              <h4>
-                <b>{bike.brand}</b>
-              </h4>
+            <img
+              src={bike.image}
+              alt="Avatar"
+              style={{ height: "17rem", width: "17rem" }}
+            />
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                flexDirection: "column",
+              }}
+            >
+         
+                <h4>
+                  <b>{bike.brand}</b>
+                </h4>
               </Box>
-              <Box>
-              <p>{bike.origin}</p>
-              </Box>
-            </Box>
-
-            <Button>Click here</Button>
+          
           </Box>
         );
       })}
