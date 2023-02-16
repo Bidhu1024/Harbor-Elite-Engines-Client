5e68fa9d1b9241d9a6430e864289bf356a9b4670
Bidhu1024 <bidhu1024av@gmail.com>
2023-02-16 07:39:20 +0530

    Ev API integrated

--- src/components/Cars/CarBrands.jsx
+++ src/components/Cars/CarBrands.jsx
@@ -1,51 +1,80 @@
-import React from 'react'
-import {Box} from '@mui/material'
+import React from "react";
+import { Box } from "@mui/material";
 import { Circles } from "react-loader-spinner";
 import { useQuery } from "react-query";
 import axios from "axios";
-import { Typography } from '@mui/material';
+import { Typography } from "@mui/material";
 const CarBrands = () => {
-
-  const getCarBrands = async ()=>{
-    const res =await axios.get('/allcars')
-    return res.data
+  const getCarBrands = async () => {
+    const res = await axios.get("/allcars");
+    return res.data;
     // console.log(res);
-   
-  }
+  };
 
-  const {data, error,isLoading} = useQuery('car-brands',getCarBrands )
-// console.log(data)
-if(data!==null){
-  console.log(data);
-}
- console.log(error);
- if (isLoading) {
+  const { data, error, isLoading } = useQuery("car-brands", getCarBrands);
+  // console.log(data)
+  if (data !== null) {
+    console.log(data);
+  }
+  console.log(error);
+  if (isLoading) {
+    return (
+      <Circles
+        height="150"
+        width="100%"
+        color="#4fa94d"
+        ariaLabel="circles-loading"
+        wrapperStyle={{}}
+        wrapperclassName=""
+        visible={true}
+      />
+    );
+  }
   return (
-    <Circles
-      height="150"
-      width="100%"
-      color="#4fa94d"
-      ariaLabel="circles-loading"
-      wrapperStyle={{}}
-      wrapperclassName=""
-      visible={true}
-    />
-  );
-}
-  return (
-    <Box sx={{ width: "100%", height: "100%", display: "flex", gap:'1rem',flexWrap:'wrap',justifyContent:'space-between',padding:'10px' }}>
+    <Box
+      sx={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        gap: "1rem",
+        flexWrap: "wrap",
+        justifyContent: "space-between",
+        padding: "10px",
+      }}
+    >
       {data &&
         data.map((car) => {
           return (
-          <Box sx={{display:'flex', flexDirection:'column', border:'1px solid black',boxShadow: 3, borderRadius:'5px' }}>
-          <Box component='img' src = {car.image} sx={{width:'20rem', height:'10rem'}}/>
-          <Typography sx={{fontSize:'1rem', color:'blue', fontWeight:'600', display:'flex', justifyContent:'center'}}>{car.brand}</Typography>
-
-          </Box>
-          )
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                border: "1px solid black",
+                boxShadow: 3,
+                borderRadius: "5px",
+              }}
+            >
+              <Box
+                component="img"
+                src={car.image}
+                sx={{ width: "20rem", height: "10rem" }}
+              />
+              <Typography
+                sx={{
+                  fontSize: "1rem",
+                  color: "blue",
+                  fontWeight: "600",
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+              >
+                {car.brand}
+              </Typography>
+            </Box>
+          );
         })}
     </Box>
   );
-}
+};
 
-export default CarBrands
+export default CarBrands;
