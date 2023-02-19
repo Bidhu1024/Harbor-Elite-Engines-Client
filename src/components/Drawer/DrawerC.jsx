9edce9fa99ef6a01d9a26df818b4be008f771309
Bidhu1024 <bidhu1024av@gmail.com>
2023-02-19 09:26:30 +0530

    Drawer Design Completed. Add routes

--- src/components/Drawer/DrawerC.jsx
+++ src/components/Drawer/DrawerC.jsx
@@ -1,47 +1,56 @@
-import React from 'react'
-import {Drawer,Box,Typography} from '@mui/material';
-import { useState } from 'react';
-import Menu from '@mui/icons-material/Menu';
-import TwoWheelerIcon from '@mui/icons-material/TwoWheeler';
+import React from "react";
+import { Drawer, Box, Typography } from "@mui/material";
+import { useState } from "react";
+import Menu from "@mui/icons-material/Menu";
+import TwoWheelerIcon from "@mui/icons-material/TwoWheeler";
+import AirportShuttleIcon from '@mui/icons-material/AirportShuttle';
+import bike from '../../Assets/pngs/bike.png'
+import ElectricBikeIcon from '@mui/icons-material/ElectricBike';
 
 const DrawerC = () => {
-  const [isOpen,setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
   const Admins = [
     {
-      id:1,
-      name:"Bike Admin",
-      icon:'<TwoWheelerIcon/>',
+      id: 1,
+      name: "Bike Admin",
+      icon: <TwoWheelerIcon/>,
     },
     {
-      id:2,
-      name:"Car Admin",
-      icon:'<TwoWheelerIcon/>',
+      id: 2,
+      name: "Car Admin",
+      icon: <AirportShuttleIcon/>,
     },
     {
-      id:3,
-      name:"EVs Admin",
-      icon:'<TwoWheelerIcon/>',
+      id: 3,
+      name: "EVs Admin",
+      icon: <ElectricBikeIcon/>,
     },
-  ]
+  ];
   return (
     <>
-    <Menu onClick={()=>setIsOpen(true)} />
-    <Drawer anchor='right' open={isOpen} onClose={()=>setIsOpen(false)}>
-    <Box p={2} width="250px" textAlign="center" role="presentation" sx={{backgroundColor:'teal', height:'100%'}}>
-    <Box>
-    {Admins.map((vehicle, id)=>{
-      return(
-      <Box key={id}>
-        {vehicle.name}
-      </Box>
-      )
-    })}
-    </Box>
-    </Box>
-    </Drawer>
+      <Menu onClick={() => setIsOpen(true)} />
+      <Drawer anchor="right" open={isOpen} onClose={() => setIsOpen(false)}>
+        <Box
+          p={2}
+          width="250px"
+          textAlign="center"
+          role="presentation"
+          sx={{ backgroundColor: "teal", height: "100%", display:'flex', flexDirection:'column', gap:'1rem' }}
+        >
+        <Typography fontFamily='roboto' fontWeight='500' color='white' fontSize='1rem'>Admin Panel</Typography>
+          {Admins.map((vehicle, id) => {
+            return  ( 
+             <Box key={vehicle.id} sx={{boxShadow:2,display:'flex', backgroundColor:'white',padding:'.4rem', justifyContent:'center', gap:'4px', borderRadius:'6px', cursor:'pointer' }}>
+             {vehicle.icon}
+              <Typography>{vehicle.name}</Typography>
+            </Box>
+            )
+          })}
+        </Box>
+      </Drawer>
     </>
-  )
-}
+  );
+};
 
 export default DrawerC;
