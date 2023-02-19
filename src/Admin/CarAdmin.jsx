5deb493f39ab3b96295e89d4c487393c65b8d200
Bidhu1024 <bidhu1024av@gmail.com>
2023-02-19 20:32:36 +0530

    Design Car page

--- src/Admin/CarAdmin.jsx
+++ src/Admin/CarAdmin.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import {Box} from '@mui/material'
+import { TextField } from '@mui/material';
 const CarAdmin = () => {
   return (
-    <div>CarAdmin</div>
+    <Box sx={{display:'flex',flexDirection:"column", alignItems:'space-between'}}>
+      <TextField size='small' label="Brand Name"/>
+      <TextField size='small' label="Car Name"/>
+      <TextField size='small' label="Brand Name"/>
+      <TextField size='small' label="Brand Name"/>
+      <TextField size='small' label="Brand Name"/>
+    </Box>
   )
 }
 
