c64257055ce69cea308f62eacc4b3b495fc94f36
Bidhu1024 <bidhu1024av@gmail.com>
2023-02-23 22:18:39 +0530

    kali karibi

--- src/Admin/BikeAdmin.jsx
+++ src/Admin/BikeAdmin.jsx
@@ -7,8 +7,9 @@ import React, { useState } from "react";
 const BikeAdmin = () => {
   const [age, setAge] = useState("");
   const [files, setFiles] = useState("");
-  const [name, setName] = useState("");
+  const [brand, setBrand] = useState("");
   const [error, setError] = useState(false);
+  const [name, setName] = useState("");
   const handleUpload = async (e) => {
     e.preventDefault();
     try {
@@ -36,10 +37,14 @@ const BikeAdmin = () => {
   const handleChange = (event) => {
     setAge(event.target.value);
   };
-  const handleName  = (e)=>{
+  const handleBrand = (e) => {
+    setBrand(e.target.value);
+    setError(!e.target.value);
+  };
+  const handleName = (e) => {
     setName(e.target.value)
-    setError(!e.target.value)
-  }
+    setError(e.target.value.length<=4)
+  };
   return (
     <Box sx={{ width: "100%", height: "100%" }}>
       <Typography
@@ -85,18 +90,25 @@ const BikeAdmin = () => {
                 id="outlined-basic"
                 variant="outlined"
                 required
-                error = {error}
-                value = {name}
-                onChange = {handleName}
+                error={error}
+                value={brand}
+                onChange={handleBrand}
                 size="small"
-                helperText={error ? 'Name is required' : ''}
+                helperText={error ? "BrandName is required" : ""}
               />
             </Box>
             <Box sx={{ display: "flex", alignItems: "center", gap: "1.2rem" }}>
               <Typography fontSize="1.2rem">
                 Enter the Name of the Bike
               </Typography>
-              <TextField id="outlined-basic" variant="outlined" size="small" />
+              <TextField id="outlined-basic" variant="outlined" size="small"
+              required
+              error={error}
+                value={name}
+                onChange={handleName}
+                
+                helperText={error ? "Bike Name is required" : ""}
+               />
             </Box>
 
             <Box sx={{ display: "flex", alignItems: "center", gap: "1.2rem" }}>
