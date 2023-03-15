e25f6ed3a23f820b36a120c1c0b95db8d5f887e2
Bidhu1024 <bidhu1024av@gmail.com>
2023-03-15 21:49:56 +0530

    add img and form

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 
 const SignIn = () => {
   return (
-    <Box sx={{height:'100vh', width:'100vw', backgroundColor:'#eab5eb', display:'flex', justifyContent:'center', alignItems:'center', opacity:'.5'}}>
-    <Box>
-    <Box>image</Box>
-    <Box>form</Box>
+    <Box sx={{height:'100vh', width:'100vw', backgroundColor:'#eab5eb', display:'flex', justifyContent:'center', alignItems:'center', opacity:'.7'}}>
+    <Box sx={{display:'flex',border:'1px solid black', width:'35rem', height:'30rem'}}>
+    <Box sx={{width:'50%', height:'100%'}}>form</Box>
+    <Box sx={{width:'50%', height:'100%'}}>image</Box>
     </Box>
     </Box>
   )
