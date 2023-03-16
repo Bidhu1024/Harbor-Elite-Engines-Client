863341c55c63c9a01a01490d50595347733a8468
Bidhu1024 <bidhu1024av@gmail.com>
2023-03-16 07:09:45 +0530

    jjj

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,15 +1,40 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import { Box, Typography } from "@mui/material";
+import React from "react";
+import loginlogo from "../Assets/logologin.jpeg";
 
 const SignIn = () => {
   return (
-    <Box sx={{height:'100vh', width:'100vw', backgroundColor:'#eab5eb', display:'flex', justifyContent:'center', alignItems:'center', opacity:'.7'}}>
-    <Box sx={{display:'flex',border:'1px solid black', width:'35rem', height:'30rem'}}>
-    <Box sx={{width:'50%', height:'100%'}}>form</Box>
-    <Box sx={{width:'50%', height:'100%'}}>image</Box>
+    <Box
+      sx={{
+        height: "100vh",
+        width: "100vw",
+        backgroundColor: "#eab5eb",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        opacity: ".7",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          border: "1px solid black",
+          width: "40rem",
+          height: "30rem",
+          boxShadow: 3,
+        }}
+      >
+        <Box sx={{ width: "50%", height: "100%", backgroundColor: "white" }}>
+          <Typography fontSize="2rem" textAlign="center">
+            Login
+          </Typography>
+        </Box>
+        <Box sx={{ width: "50%", height: "100%" }}>
+          <img src={loginlogo} width="320" height="480" objectFit="cover" />{" "}
+        </Box>
+      </Box>
     </Box>
-    </Box>
-  )
-}
+  );
+};
 
 export default SignIn;
