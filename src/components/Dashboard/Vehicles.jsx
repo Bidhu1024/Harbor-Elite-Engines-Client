0519427b65f32b4b04288a1684838bea4afb173b
Bidhu1024 <bidhu1024av@gmail.com>
2023-03-19 10:29:50 +0530

    q

--- src/components/Dashboard/Vehicles.jsx
+++ src/components/Dashboard/Vehicles.jsx
@@ -3,6 +3,8 @@ import React from "react";
 import TwoWheelerIcon from '@mui/icons-material/TwoWheeler';
 import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
 import ElectricCarIcon from '@mui/icons-material/ElectricCar';
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import QueryStatsIcon from '@mui/icons-material/QueryStats';
 const Vehicles = () => {
   return (
     <Box sx={{ width: "100%", display: "flex", justifyContent: "center" }}>
@@ -18,22 +20,22 @@ const Vehicles = () => {
         }}
       >
         <Box sx={{ height: "8rem", width: "33%", backgroundColor: "#36454f", display:'flex', alignItems:'center', justifyContent:'center' }}>
-          <Typography sx={{marginRight:'4px'}}>Bikes</Typography> <TwoWheelerIcon  />
+          <Typography sx={{marginRight:'4px', fontSize:'1.2rem', color:'white'}}>Bikes</Typography> <TwoWheelerIcon sx={{color:'yellow'}} />
         </Box>
         <Box sx={{ height: "8rem", width: "33%", backgroundColor: "#36454f", display:'flex', alignItems:'center', justifyContent:'center' }}>
-          <Typography sx={{marginRight:'4px'}}>Cars</Typography><DirectionsCarIcon />
+          <Typography sx={{marginRight:'4px', fontSize:'1.2rem', color:'white'}}>Cars</Typography><DirectionsCarIcon sx={{color:'yellow'}} />
         </Box>
         <Box sx={{ height: "8rem", width: "33%", backgroundColor: "#36454f", display:'flex', alignItems:'center', justifyContent:'center' }}>
-          <Typography sx={{marginRight:'4px'}}>Electric Vehicles</Typography><ElectricCarIcon />
+          <Typography sx={{marginRight:'4px', fontSize:'1.2rem', color:'white'}}>Electric Vehicles</Typography><ElectricCarIcon sx={{color:'yellow'}} />
         </Box>
         <Box sx={{ height: "8rem", width: "33%", backgroundColor: "#36454f", display:'flex', alignItems:'center', justifyContent:'center' }}>
-          <Typography>New Launches</Typography>
+          <Typography fontSize="1.2rem" color="white" >New Launches 2023</Typography>
         </Box>
         <Box sx={{ height: "8rem", width: "33%", backgroundColor: "#36454f", display:'flex', alignItems:'center', justifyContent:'center' }}>
-          <Typography>Trendings</Typography>
+          <Typography fontSize="1.2rem" color="white">Trendings</Typography><TrendingUpIcon sx={{color:'yellow'}} />
         </Box>
         <Box sx={{ height: "8rem", width: "33%", backgroundColor: "#36454f", display:'flex', alignItems:'center', justifyContent:'center' }}>
-          <Typography>Stats</Typography>
+          <Typography fontSize="1.2rem" color="white">Stats</Typography><QueryStatsIcon sx={{color:'yellow'}}  />
         </Box>
       </Box>
     </Box>
