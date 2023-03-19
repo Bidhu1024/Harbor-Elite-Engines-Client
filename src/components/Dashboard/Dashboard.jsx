5b0bfd51097c5b1e3929189929ae0effef09b49d
Bidhu1024 <bidhu1024av@gmail.com>
2023-03-19 09:13:44 +0530

    user box completed

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -9,12 +9,14 @@ import EVBox from "./EVBox";
 import Recommendations from "./Recommendations";
 import Topbar from "./Topbar";
 import Users from "./Users";
+import Vehicles from "./Vehicles";
 const Dashboard = () => {
   return (
     <>
     <Stack sx={{ width: "100vw", height:'100vh', backgroundColor:'#28282B' }}>
     <Topbar/>
     <Users/>
+    <Vehicles />
     </Stack>
       {/* <Box
         sx={{
--- src/components/Dashboard/Users.jsx
+++ src/components/Dashboard/Users.jsx
@@ -1,17 +1,38 @@
-import { Box,Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import React from "react";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 
 const Users = () => {
   return (
-   <Box sx={{display:'flex', justifyContent:'center',width:'100%',}}>
-     <Box sx={{width:'60%',display:'flex', justifyContent:'space-between',alignItems:'center', marginTop:'6rem'}}>
-      <Box sx={{display:'flex', flexDirection:'column'}}>
-        <Box><Typography fontSize="3rem" color='whitesmoke'>USERNAME</Typography></Box>
-        <Box>Welcome </Box>
+    <Box sx={{ display: "flex", justifyContent: "center", width: "100%" }}>
+      <Box
+        sx={{
+          width: "70%",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          marginTop: "4rem",
+          boxShadow:2,
+          padding:'10px'
+        }}
+      >
+        <Box sx={{ display: "flex", flexDirection: "column" }}>
+          <Box>
+            <Typography fontSize="3rem" color="whitesmoke">
+              Hey USERNAME
+            </Typography>
+          </Box>
+          <Box>
+            <Typography fontSize="1rem" color="whitesmoke">
+              Welcome to Harbor Elite Engines.
+            </Typography>{" "}
+          </Box>
+        </Box>
+        <Box>
+          <FavoriteBorderIcon />
+        </Box>
       </Box>
-      <Box>Favs section</Box>
     </Box>
-   </Box>
   );
 };
 
