a9aad5436eea354f5e7e47b35ff25699b31082eb
Bidhu1024 <bidhu1024av@gmail.com>
2023-03-19 09:00:53 +0530

    User section started

--- src/components/Dashboard/Users.jsx
+++ src/components/Dashboard/Users.jsx
@@ -1,15 +1,17 @@
-import { Box } from "@mui/material";
+import { Box,Typography } from "@mui/material";
 import React from "react";
 
 const Users = () => {
   return (
-    <Box sx={{width:'100%',display:'flex', justifyContent:'space-between', marginTop:'6rem'}}>
+   <Box sx={{display:'flex', justifyContent:'center',width:'100%',}}>
+     <Box sx={{width:'60%',display:'flex', justifyContent:'space-between',alignItems:'center', marginTop:'6rem'}}>
       <Box sx={{display:'flex', flexDirection:'column'}}>
-        <Box>Username</Box>
+        <Box><Typography fontSize="3rem" color='whitesmoke'>USERNAME</Typography></Box>
         <Box>Welcome </Box>
       </Box>
       <Box>Favs section</Box>
     </Box>
+   </Box>
   );
 };
 
